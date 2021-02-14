7726e7f982437796e3fdd666549bcdcb3e9ff8aa
qikaqika <78652924+qikaqika@users.noreply.github.com>
2021-02-14 10:00:09 +0700

    Rename Two Piece v2.lua to TwoPiecev2.lua

--- Two Piece v2.lua
+++
@@ -1,2 +0,0 @@
-getfenv().lol2 = 1000
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tiurabe/roblox/master/TwoPieceAlternative.lua",true))()
---
+++ TwoPiecev2.lua
@@ -0,0 +1,2 @@
+getfenv().lol2 = 1000
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tiurabe/roblox/master/TwoPieceAlternative.lua",true))()
