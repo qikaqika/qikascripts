ffe98719850aa349fad7d4f909c28e5f51a134a6
qikaqika <78652924+qikaqika@users.noreply.github.com>
2021-02-14 10:00:32 +0700

    Rename Two Piece.lua to TwoPiece.lua

--- Two Piece.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ogamertv12/FreeScript/main/TwoPiece.lua",true))()
---
+++ TwoPiece.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ogamertv12/FreeScript/main/TwoPiece.lua",true))()
