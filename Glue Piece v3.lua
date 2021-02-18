52af16621dfdae7453d9a8bb86a8718c985d9187
qikaqika <78652924+qikaqika@users.noreply.github.com>
2021-02-19 03:00:13 +0700

    Add files via upload

---
+++ Glue Piece v3.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/trashss/trashs-scripts-obfuscated/main/Glue%20Piece", true))()
---
+++ King Piece.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/AltsegoD/script/master/KingPiece.lua')))()
